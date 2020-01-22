88a61914ab21f85a15d19ddc1386807889ee9c40
Ben Darwin <bcdarwin@gmail.com>
2020-01-22 14:01:58 -0500

    python3Packages.minc_stuffs: init at 0.1.25

---
+++ python-modules/minc-stuffs.nix
@@ -0,0 +1,36 @@
+{ buildPythonPackage, fetchFromGitHub, stdenv, cmake,
+   minc_tools, mni_autoreg, pyminc, netcdf, zlib, hdf5, bicpl,
+  numpy, scipy, pytest, m4, autoconf, automake }:
+
+# TODO split this into a c/perl and python package akin to minc2_simple ...
+
+buildPythonPackage rec {
+  pname   = "minc-stuffs";
+  version = "0.1.25";
+
+  src = fetchFromGitHub {
+    owner = "Mouse-Imaging-Centre";
+    repo  = pname;
+    rev   = "v${version}";
+    sha256 = "00dkd8sf5gr5fwzbjrmw0fc0lyvhdb4n9mmrhmfdm6ss0fbznalj";
+  };
+
+  # not sure if there's a better way to do this ...
+  preConfigure = "./autogen.sh";
+  preBuild = "make";
+  postInstall = "make install";
+
+  nativeBuildInputs = [ m4 automake autoconf ];
+  buildInputs = [ netcdf hdf5 bicpl zlib ];
+  propagatedBuildInputs = [ numpy scipy pyminc ];
+
+  doCheck = false;  # no tests?!
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/Mouse-Imaging-Centre/minc-stuffs";
+    description = "Various MINC programs and scripts";
+    maintainers = with maintainers; [ bcdarwin ];
+    platforms = platforms.linux;
+    license   = licenses.bsd3;
+  };
+}
--- python-packages.nix
+++ python-packages.nix
@@ -4,6 +4,8 @@ let
   callPackage = super.callPackage;
 in
 {
+  minc_stuffs = callPackage python-modules/minc-stuffs.nix { };
+
   pyminc = callPackage python-modules/pyminc.nix { };
 
   qbatch = callPackage python-modules/qbatch.nix { };
