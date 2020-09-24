5bed4dc2d115ca746b952bc318f2a4d884c4a419
cfhammill <cfhammill@gmail.com>
2020-09-24 17:02:48 -0400

    RMINC: fix build
    Changes:
    - needs up to date RMINC develop (fixes several cran warnings)
    - needs the correct test data rminctestdata2.tar.gz
    - needs checkbashisms (fixes a cran warnings)
    - reorders the installCheckPhase to avoid a potential top level
      note
    - uses runRMINCTestbed instead of `devtools` check because
      it seems to ignore `RMINC_DATA_DIR`
    - adds a few comments

--- pkgs/RMINC.nix
+++ pkgs/RMINC.nix
@@ -1,5 +1,7 @@
-{ lib, rPackages, fetchFromGitHub, fetchzip
-, zlib, hdf5, libminc, conglomerate, minc_tools, minc_stuffs }:
+{ lib, R, rPackages, fetchFromGitHub, fetchzip
+  , zlib, hdf5, libminc, conglomerate, minc_tools, minc_stuffs
+  , pandoc, checkbashisms
+}:
 
 rPackages.buildRPackage rec {
   pname   = "RMINC";
@@ -9,26 +11,31 @@ rPackages.buildRPackage rec {
   src = fetchFromGitHub {
     owner = "Mouse-Imaging-Centre";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "0vl5yk48c1fm8szxvpbaqy8q2qwcllksq4zvb7rcn2dw8g6nbg63";
+    rev = "d1b5647d6c47a94b3e613292067e45fda732c259";
+    sha256 = "1lqa7r0rlwwprpcxrwz7q1vcgy3s6bmmkim90khzd34lh3k9kgm5";
   };
 
   test_data = fetchzip {
-    url = "https://wiki.mouseimaging.ca/download/attachments/1654/rminctestdata.tar.gz";
-    sha256 = "1a51s3zp5arh0qljhsdn8jc0sk1xiy2h0sispnr1qm0bkrbazdy3";
+    url = "https://wiki.mouseimaging.ca/download/attachments/1654/rminctestdata2.tar.gz";
+    sha256 = "1nrz2aa4h8zc1iqxx4yjnnk9im4cvs5faglcnbrq6h3si8scfqkb";
   };
 
-  propagatedBuildInputs = [ zlib hdf5 libminc minc_tools minc_stuffs conglomerate ] ++
+  propagatedBuildInputs = [ R zlib hdf5 libminc minc_tools minc_stuffs conglomerate pandoc ] ++
   (with rPackages; [
     tidyverse lme4 batchtools Rcpp
     shiny gridBase data_tree visNetwork rjson DT
     rgl igraph qvalue lmerTest bigstatsr
+    devtools
   ]);
 
-  installCheckInputs = [ rPackages.testthat ];
+  installCheckInputs = [ checkbashisms rPackages.testthat ];
 
   installCheckPhase = ''
     R CMD build .
+    R CMD check --no-manual --as-cran --no-install RMINC*.tar.gz
+    cat RMINC.Rcheck/00check.log         
+
+    # Set up test data
     (mkdir build_tmp  
      cd build_tmp
      cp -r ${test_data} rminctestdata
@@ -36,17 +43,20 @@ rPackages.buildRPackage rec {
      tar -czf rminctestdata.tar.gz rminctestdata
      cp rminctestdata.tar.gz ..
     )
-    export TEST_Q_MINC="no"
-    export RMINC_DATA_DIR=$(pwd)
-    R CMD check --as-cran --no-install RMINC*.tar.gz
-    cat RMINC.Rcheck/00check.log         
+
+    # Run the test bed
+    export RMINC_DATA_DIR=$PWD
     Rscript -e "\
-    testr <- devtools::test(); \
+    library(RMINC); \
+    options(stringsAsFactors = TRUE); \
+    testr <- RMINC::runRMINCTestbed(test_q_minc = FALSE); \
     testr <- as.data.frame(testr); \
     if(any(testr\$error) || any(testr\$warning > 0)) \
       stop('Found failing tests') \
     "
     pass=$?
+
+    # Report pass if all tests pass and no warnings or errors in check
     if [[ $pass -ne 0 || $(grep "WARNING\|ERROR" RMINC.Rcheck/00check.log) != "" ]]; then
       (exit 1)
     else
@@ -56,7 +66,7 @@ rPackages.buildRPackage rec {
   # not the usual way to test packages, but `R CMD check .` by itself fails on the DESCRIPTION file,
   # and `R CMD build .; R CMD check ./RMINC_1.5.2.2.tar.gz` triggers a rebuild from the tarball
 
-  #doInstallCheck = true;
+  doInstallCheck = true;
 
   meta = with lib; {
     homepage = "https://github.com/Mouse-Imaging-Centre/RMINC";
