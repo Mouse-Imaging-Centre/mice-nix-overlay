bc24361c640e8dc201c91d7adb8d3fa376085e79
Ben Darwin <bcdarwin@gmail.com>
2021-02-01 16:11:50 -0500

    add Python 3.9

--- default.nix
+++ default.nix
@@ -7,11 +7,13 @@ rec {
 
   python37 = super.python37.override { packageOverrides = import ./python-packages.nix; };
   python38 = super.python38.override { packageOverrides = import ./python-packages.nix; };
+  python39 = super.python39.override { packageOverrides = import ./python-packages.nix; };
   python3 = python38;
 
   # can we define python3Packages to refer to the version defined in Nixpkgs?
   python37Packages = super.recurseIntoAttrs python37.pkgs;
   python38Packages = super.recurseIntoAttrs python38.pkgs;
+  python39Packages = super.recurseIntoAttrs python39.pkgs;
   python3Packages = python38Packages;
 
   # should this be wrapped in toPythonApplication?
